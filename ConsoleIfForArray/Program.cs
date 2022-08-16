4a1f663116df65ea04e6e368709138b9a2ec91ad
novakvova <novakvova@gmail.com>
2022-08-16 19:14:48 +0300

    Begin array

---
+++ ConsoleIfForArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleIfForArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            //int, double, float, string, char, short, bool, unsigned int,
+            //decimal, 
+            //2^16 = 
+            int n = 5;
+            int [] mas=new int[n];
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(2,10);
+            }
+            foreach (int item in mas.Where(x=>x>5))
+            {
+                //if(item>5)
+                    Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+            foreach (int item in mas.Reverse<int>())
+                Console.Write(item+"\t");
+            
+        }
+    }
+}
